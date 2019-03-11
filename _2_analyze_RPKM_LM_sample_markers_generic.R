ab9f575d63998ca006005e48b5c3fb0d0eba5a99
aylaergun <48434717+aylaergun@users.noreply.github.com>
2019-03-11 05:41:32 -0400

    Add files via upload

---
+++ _2_analyze_RPKM_LM_sample_markers_generic.R
@@ -0,0 +1,149 @@
+rm(list=ls())
+fdir<-getwd();
+
+#read in expression from TCGA: "normalized_count"(quantile normalized RSEM) value 
+#from illumina hiseq/ga2 mRNAseq level_3 (v2) data released on 20160228.
+
+RSEM_Gene_IlluminaHiSeq<-read.table(paste(fdir,"/CancerType.RSEM_genes_normalized.txt",sep=""),header=TRUE,sep="\t");
+
+AllData<-RSEM_Gene_IlluminaHiSeq[,2:ncol(RSEM_Gene_IlluminaHiSeq)];
+names1<-RSEM_Gene_IlluminaHiSeq[2:20532,1];
+names2=as.character(names1);
+names=matrix(nrow=length(names2),ncol=1);
+
+for (i in 1:length(names1))
+{names3=strsplit(names2[i],"|",fixed=TRUE);
+ names[i]=names3[[1]][1];
+}
+
+
+RPKM<-matrix(nrow=20531,ncol=ncol(RSEM_Gene_IlluminaHiSeq)-1);
+sample_names<-matrix(nrow=ncol(RSEM_Gene_IlluminaHiSeq)-1,ncol=1);
+for (i in 1:(ncol=ncol(RSEM_Gene_IlluminaHiSeq)-1))
+{
+RPKM[,i]<-as.numeric(as.matrix(AllData[2:20532,i])); 
+sample_names[i]<-colnames(RSEM_Gene_IlluminaHiSeq)[i+1];
+}
+rownames(RPKM)<-unlist(names);
+colnames(RPKM)<-sample_names;
+
+new_sample_names=matrix(nrow=ncol(RSEM_Gene_IlluminaHiSeq)-1,ncol=1);
+Case_Control=matrix(nrow=ncol(RSEM_Gene_IlluminaHiSeq)-1,ncol=1);
+for (i in 1:ncol(RSEM_Gene_IlluminaHiSeq)-1)
+{x=unlist(strsplit(sample_names[i], "[.]"));
+ new_sample_names[i]=paste(x[1],x[2],x[3],sep=".");
+ Case_Control[i]=x[4];
+}
+tumor_id=grep("01",Case_Control);
+
+RPKM_log2=log2(RPKM+1);
+
+#read in the immune cell signatures
+
+TCell_Sign<-read.table("TCell_Sign.txt",header=FALSE,sep="\t");
+BCell_Sign<-read.table("BCell_Sign.txt",header=FALSE,sep="\t");
+DC_Cell_Sign<-read.table("DCCell_Sign.txt",header=FALSE,sep="\t");
+Eos_Cell_Sign<-read.table("EosCell_Sign.txt",header=FALSE,sep="\t");
+MPhage_Cell_Sign<-read.table("MPhageCell_Sign.txt",header=FALSE,sep="\t");
+Neutrophil_Cell_Sign<-read.table("NeutrophilCell_Sign.txt",header=FALSE,sep="\t");
+NK_Cell_Sign<-read.table("NKCell_Sign.txt",header=FALSE,sep="\t");
+Mast_Cell_Sign<-read.table("MastCell_Sign.txt",header=FALSE,sep="\t");
+
+TCell_index=matrix(nrow=10,ncol=length(tumor_id));
+BCell_index=matrix(nrow=10,ncol=length(tumor_id));
+DC_index=matrix(nrow=10,ncol=length(tumor_id));
+Eos_index=matrix(nrow=10,ncol=length(tumor_id));
+MPhage_index=matrix(nrow=10,ncol=length(tumor_id));
+Neutrophil_index=matrix(nrow=10,ncol=length(tumor_id));
+NK_index=matrix(nrow=10,ncol=length(tumor_id));
+Mast_index=matrix(nrow=10,ncol=length(tumor_id));
+
+#Calculate average gene expression among signature genes in tumor samples by randomly selecting 80% of the marker genes 10 times
+
+for (k in 1:10)
+{
+TCell_Genes=intersect(unlist(TCell_Sign),names);
+TCell_Genes_id=match(TCell_Genes,names);
+keep=which(apply(RPKM_log2[TCell_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+TCell_index[k,]=colMeans(RPKM_log2[TCell_Genes_id[keep[keep1]],tumor_id]);
+ 
+BCell_Genes=intersect(unlist(BCell_Sign),names);
+BCell_Genes_id=match(BCell_Genes,names);
+keep=which(apply(RPKM_log2[BCell_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+BCell_index[k,]=colMeans(RPKM_log2[BCell_Genes_id[keep[keep1]],tumor_id]);
+ 
+DC_Genes=intersect(unlist(DC_Cell_Sign),names);
+DC_Genes_id=match(DC_Genes,names);
+keep=which(apply(RPKM_log2[DC_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep1)/5),replace=FALSE);
+DC_index[k,]=colMeans(RPKM_log2[DC_Genes_id[keep[keep1]],tumor_id]);
+
+Eos_Genes=intersect(unlist(Eos_Cell_Sign),names);
+Eos_Genes_id=match(Eos_Genes,names);
+keep=which(apply(RPKM_log2[Eos_Genes_id,tumor_uid],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+Eos_index[k,]=colMeans(RPKM_log2[Eos_Genes_id[keep[keep1]],tumor_id]);
+ 
+MPhage_Genes=intersect(unlist(MPhage_Cell_Sign),names);
+MPhage_Genes_id=match(MPhage_Genes,names);
+keep=which(apply(RPKM_log2[MPhage_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+MPhage_index[k,]=colMeans(RPKM_log2[MPhage_Genes_id[keep[keep1]],tumor_id]);
+ 
+Neutrophil_Genes=intersect(unlist(Neutrophil_Cell_Sign),names);
+Neutrophil_Genes_id=match(Neutrophil_Genes,names);
+keep=which(apply(RPKM_log2[Neutrophil_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+Neutrophil_index[k,]=colMeans(RPKM_log2[Neutrophil_Genes_id[keep[keep1]],tumor_id]);
+ 
+NK_Genes=intersect(unlist(NK_Cell_Sign),names);
+NK_Genes_id=match(NK_Genes,names);
+keep=which(apply(RPKM_log2[NK_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+NK_index[k,]=colMeans(RPKM_log2[NK_Genes_id[keep[keep1]],tumor_id]);
+
+Mast_Genes=intersect(unlist(Mast_Cell_Sign),names);
+Mast_Genes_id=match(Mast_Genes,names);
+keep=which(apply(RPKM_log2[Mast_Genes_id,tumor_id],1,mean)>1);
+keep1=sample(c(1:length(keep)),4*round(length(keep)/5),replace=FALSE);
+Mast_index[k,]=colMeans(RPKM_log2[Mast_Genes_id[keep[keep1]],tumor_id]);
+}
+
+for (k in 1:10)
+{
+
+A1=rbind(RPKM_log2[,tumor_id],TCell_index[k,],BCell_index[k,],DC_index[k,],Eos_index[k,],MPhage_index[k,],Neutrophil_index[k,],NK_index[k,],Mast_index[k,]);
+rownames(A1)=c(rownames(RPKM_log2),"TCell_index","BCell_index","DC_index","Eos_index","MPhage_index","Neutrophil_index","NK_index","Mast_index");
+A2=as.data.frame(t(A1));
+
+
+Residuals_Individual<-matrix(nrow=20502,ncol=length(tumor_id));
+rownames(Residuals_Individual)=rownames(RPKM)[30:20531];
+
+X=gsub("-","",colnames(A2));
+colnames(A2)=X;
+
+
+New_RPKM1=RPKM[30:20531,];
+New_RPKM=log2(New_RPKM1[,tumor_id]+1);
+
+
+#start from 30
+
+for (i in 30:20531)
+{print(i)
+  
+  
+  form2 <- paste(colnames(A2)[i], "~","TCell_index+BCell_index+DC_index+Eos_index+MPhage_index+Neutrophil_index+NK_index+Mast_index",sep="");
+  q2=lm(formula=form2,data=A2);
+  Residuals_Individual[i-29,]=q2$residuals;
+  
+}
+
+colnames(Residuals_Individual)=colnames(RPKM[,tumor_id]);
+save("New_RPKM","Residuals_Individual",file=paste("CancerType_Residuals_Log2_Jan2018_sample_markers_v",k,".RData",sep=""));
+}
+
+
---
+++ _3_Combine_Foxp3CorrValues_SampleRandomly.R
@@ -0,0 +1,111 @@
+rm(list=ls())
+fdir<-getwd();
+
+# Load Residual Value of BRCA, COADREAD, LUAD and PAAD calculated using the full set of samples and markers.
+load("BRCA_Residuals_Log2_Jan2018.RData");
+BRCA_Before=New_RPKM;
+BRCA_After=Residuals_Individual;
+load("COADREAD_Residuals_Log2_Jan2018.RData");
+COADREAD_Before=New_RPKM;
+COADREAD_After=Residuals_Individual;
+load("LUAD_Residuals_Log2_Jan2018.RData");
+LUAD_Before=New_RPKM;
+LUAD_After=Residuals_Individual;
+load("PAAD_Residuals_Log2_Jan2018.RData");
+PAAD_Before=New_RPKM;
+PAAD_After=Residuals_Individual;
+###########################################################
+# Reshuffle data to calculate correlation as a control.
+
+BRCA_After_Rnd=matrix(0,nrow=nrow(BRCA_After),ncol=ncol(BRCA_After));
+COADREAD_After_Rnd=matrix(0,nrow=nrow(COADREAD_After),ncol=ncol(COADREAD_After));
+LUAD_After_Rnd=matrix(0,nrow=nrow(LUAD_After),ncol=ncol(LUAD_After));
+PAAD_After_Rnd=matrix(0,nrow=nrow(PAAD_After),ncol=ncol(PAAD_After));
+
+for (i in 1:nrow(BRCA_After))
+{
+  BRCA_After_Rnd[i,]=sample(BRCA_After[i,]);
+  COADREAD_After_Rnd[i,]=sample(COADREAD_After[i,]);
+  LUAD_After_Rnd[i,]=sample(LUAD_After[i,]);
+  PAAD_After_Rnd[i,]=sample(PAAD_After[i,]);
+}
+
+###########################################################
+#Merging  correlation  values  across  datasets: In  order  to  have  a  comparable  set  of 
+#correlation values for each of the 4 cancers we randomly selected a subset of equal number of 
+#samples from each cancer dataset 10 times. "rnd" refers to the correlations calculated from reshuffled data-figure S4B.
+
+a1=ncol(BRCA_Before);
+a2=ncol(COADREAD_Before);
+a3=ncol(LUAD_Before);
+a4=ncol(PAAD_Before);
+
+a=min(min(a1,a2),min(a3,a4));
+
+id=which(rownames(Residuals_Individual)=="FOXP3");
+
+BRCA_cor_before=matrix(nrow=20502,ncol=10);
+BRCA_cor_after=matrix(nrow=20502,ncol=10);
+BRCA_cor_after_rnd=matrix(nrow=20502,ncol=10);
+for (i in 1:10)
+{n=sample(a1,a,replace=FALSE);
+BRCA_cor_before[,i]=cor(t(BRCA_Before[,n]),(BRCA_Before[id,n]),use="complete.obs");
+BRCA_cor_after[,i]=cor(t(BRCA_After[,n]),BRCA_After[id,n],use="complete.obs");
+BRCA_cor_after_rnd[,i]=cor(t(BRCA_After_Rnd[,n]),BRCA_After_Rnd[id,n],use="complete.obs");
+}
+
+
+COADREAD_cor_before=matrix(nrow=20502,ncol=10);
+COADREAD_cor_after=matrix(nrow=20502,ncol=10);
+COADREAD_cor_after_rnd=matrix(nrow=20502,ncol=10);
+
+for (i in 1:10)
+{n=sample(a2,a,replace=FALSE);
+COADREAD_cor_before[,i]=cor(t(COADREAD_Before[,n]),(COADREAD_Before[id,n]),use="complete.obs");
+COADREAD_cor_after[,i]=cor(t(COADREAD_After[,n]),COADREAD_After[id,n],use="complete.obs");
+COADREAD_cor_after_rnd[,i]=cor(t(COADREAD_After_Rnd[,n]),COADREAD_After_Rnd[id,n],use="complete.obs");
+}
+
+LUAD_cor_before=matrix(nrow=20502,ncol=10);
+LUAD_cor_after=matrix(nrow=20502,ncol=10);
+LUAD_cor_after_rnd=matrix(nrow=20502,ncol=10);
+
+for (i in 1:10)
+{n=sample(a3,a,replace=FALSE);
+LUAD_cor_before[,i]=cor(t(LUAD_Before[,n]),(LUAD_Before[id,n]),use="complete.obs");
+LUAD_cor_after[,i]=cor(t(LUAD_After[,n]),LUAD_After[id,n],use="complete.obs");
+LUAD_cor_after_rnd[,i]=cor(t(LUAD_After_Rnd[,n]),LUAD_After_Rnd[id,n],use="complete.obs");
+}
+
+PAAD_corB=cor(t(PAAD_Before),(PAAD_Before[id,]),use="complete.obs");
+PAAD_corA=cor(t(PAAD_After),PAAD_After[id,],use="complete.obs");
+PAAD_corA_rnd=cor(t(PAAD_After_Rnd),PAAD_After_Rnd[id,],use="complete.obs");
+
+BRCA_corB=rowMeans(BRCA_cor_before);
+BRCA_corA=rowMeans(BRCA_cor_after);
+BRCA_corA_rnd=rowMeans(BRCA_cor_after_rnd);
+
+COADREAD_corB=rowMeans(COADREAD_cor_before);
+COADREAD_corA=rowMeans(COADREAD_cor_after);
+COADREAD_corA_rnd=rowMeans(COADREAD_cor_after_rnd);
+
+LUAD_corB=rowMeans(LUAD_cor_before);
+LUAD_corA=rowMeans(LUAD_cor_after);
+LUAD_corA_rnd=rowMeans(LUAD_cor_after_rnd);
+
+Four_Total_Before=(BRCA_corB+COADREAD_corB+LUAD_corB+PAAD_corB)/4;
+Four_Total_After=(BRCA_corA+COADREAD_corA+LUAD_corA+PAAD_corA)/4;
+Four_Total_After_rnd=(BRCA_corA_rnd+COADREAD_corA_rnd+LUAD_corA_rnd+PAAD_corA_rnd)/4;
+
+
+
+BRCA_cor=cbind(BRCA_corA,BRCA_corB);
+rownames(BRCA_cor)=rownames(Four_Total_Before);
+COADREAD_cor=cbind(COADREAD_corA,COADREAD_corB);
+rownames(COADREAD_cor)=rownames(Four_Total_Before);
+LUAD_cor=cbind(LUAD_corA,LUAD_corB);
+rownames(LUAD_cor)=rownames(Four_Total_Before);
+PAAD_cor=cbind(PAAD_corA,PAAD_corB);
+rownames(PAAD_cor)=rownames(Four_Total_Before);
+FourC_cor=cbind(Four_Total_After,Four_Total_Before);
+
